8f6dcab56a8a15fb6d0c0207c71501b2583f27ae
Tanik <janik.junginger@gmail.com>
2025-06-25 12:11:50 +0200

    Added first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod union_find;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::union_find::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn construction() {
+        UnionFind::new(10);
     }
 }
---
+++ src/union_find.rs
@@ -0,0 +1,39 @@
+#[derive(PartialEq,Eq)]
+pub struct Representative {value: usize}
+#[derive(Debug)]
+pub struct UnionFind {
+    parents: Vec<usize>
+}
+    
+impl UnionFind {
+    pub fn new(size: usize) -> UnionFind {
+        UnionFind{parents: vec![size+1;size]}
+    }
+    // no bounds check
+    pub fn find(&mut self, e: usize) -> Representative {
+        if self.parents[e] > self.parents.len() {
+            return Representative{value: e};
+        }
+        let r = self.find(self.parents[e]);
+        self.parents[e] = r.value;
+        return r;
+    }
+    pub fn union(&mut self, r1: Representative, r2: Representative) {
+        let i = r1.value;
+        let j = r2.value;
+        if self.parents[i] < self.parents[j] {
+            self.parents[i] = j;
+        } else if self.parents[i] > self.parents[j] {
+            self.parents[j] = i;
+        } else {
+            self.parents[j] = i;
+            self.parents[i] += 1;
+        }
+    }
+    pub fn contains(&mut self, r: &Representative, e: usize) -> bool {
+        self.find(e) == *r
+    }
+	pub fn in_same(&mut self, e1: usize, e2: usize) -> bool {
+		self.find(e1) == self.find(e2)
+	}
+}
